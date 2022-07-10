ffb814487217b0bcbe5984d6b95c65713706bdc1
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2022-07-10 19:37:15 -0400

    Update ViewController.swift
    added self

--- KadaiPartOne/ViewController.swift
+++ KadaiPartOne/ViewController.swift
@@ -19,21 +19,22 @@ class ViewController: UIViewController {
 
     @IBAction private func sumButton(_ sender: Any) {
         // Convert textField.text to double
-        let textNum1 = (textField1.text! as NSString).doubleValue
-        let textNum2 = (textField2.text! as NSString).doubleValue
-        let textNum3 = (textField3.text! as NSString).doubleValue
-        let textNum4 = (textField4.text! as NSString).doubleValue
-        let textNum5 = (textField5.text! as NSString).doubleValue
+        let textNum1 = (self.textField1.text! as NSString).doubleValue
+        let textNum2 = (self.textField2.text! as NSString).doubleValue
+        let textNum3 = (self.textField3.text! as NSString).doubleValue
+        let textNum4 = (self.textField4.text! as NSString).doubleValue
+        let textNum5 = (self.textField5.text! as NSString).doubleValue
 
         // Add all textField numbers
         let totalNum = textNum1 + textNum2 + textNum3 + textNum4 + textNum5
 
         // Set "Label" when totalNum is 0.0 otherwise set totalNum
         if totalNum == 0.0 {
-            totalLabel.text = "Label"
+            self.totalLabel.text = "Label"
         }else{
-            totalLabel.text = "\(totalNum)"
+            self.totalLabel.text = "\(totalNum)"
         }
+
     }
 
 }
